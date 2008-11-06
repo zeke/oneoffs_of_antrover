5c098844e3b2b1521e1b4176a978fdafcf20fd8a
Dave Hoefler <david.hoefler@punchstock.com>
2008-11-06 15:08:40 -0600

    Adding some error handling if the original image size isnt unavailable

--- flickr_photos_in_a_set.rb
+++ flickr_photos_in_a_set.rb
@@ -24,21 +24,27 @@ class PhotosInASet
   def pull_photos
     photos = flickr.photosets.getPhotos(:photoset_id => self.set_number, :per_page => '2000', :page => '1')
     photos.photo.each do |photo|
-      available_image_sizes = flickr.photos.getSizes(:photo_id => photo.id)
-      original_image_url = available_image_sizes.find {|x| x.label == "Original"}.source.gsub("\\","").gsub(" ", "")
-      unless File.exist?(File.join(self.save_directory,File.basename(original_image_url)))
-        puts "Downloading: #{original_image_url}"
-        url = URI.parse(original_image_url)
-        req = Net::HTTP::Get.new(url.path)
-        res = Net::HTTP.start(url.host, url.port) {|http|http.request(req)}
-        File.open(File.join(self.save_directory,File.basename(original_image_url)), 'wb') do |file|
-          file.write(res.body)
+      begin
+        available_image_sizes = flickr.photos.getSizes(:photo_id => photo.id)
+        original_image_url = available_image_sizes.find {|x| x.label == "Original"}.source.gsub("\\","").gsub(" ", "")
+        unless File.exist?(File.join(self.save_directory,File.basename(original_image_url)))
+          puts "Downloading: #{original_image_url}"
+          url = URI.parse(original_image_url)
+          req = Net::HTTP::Get.new(url.path)
+          res = Net::HTTP.start(url.host, url.port) {|http|http.request(req)}
+          File.open(File.join(self.save_directory,File.basename(original_image_url)), 'wb') do |file|
+            file.write(res.body)
+          end
+        else
+          puts "Skipping #{original_image_url}"
         end
-      else
-        puts "Skipping #{original_image_url}"
+        # Shouldn't have to do this shizzz... but it cuts memory useage in half! (on my box: MBP 2.667 2gb RAM)
+        GC.start
+      rescue NoMethodError => e
+        # this will happen if 'source' isn't in the returned XML because the user decided to hide the original size
+        puts "Photo unavailable - #{photo.id}"
+        next
       end
-      # Shouldn't have to do this shizzz... but it cuts memory useage in half! (on my box: MBP 2.667 2gb RAM)
-      GC.start
     end
   end
 
