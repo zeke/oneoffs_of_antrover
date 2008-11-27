2506b940ff65f6672b15094b5a561681c1c0435b
Dave Hoefler <dhoefler@gmail.com>
2008-11-26 22:10:41 -0600

    Adding error handleing so this script wont die

--- flickr/flickr_all_photos_in_group.rb
+++ flickr/flickr_all_photos_in_group.rb
@@ -4,7 +4,13 @@
 # TODO: Document this script.
 
 require 'rubygems'
-require 'flickraw'
+begin
+  require 'flickraw'
+rescue Exception => e
+  puts "flickr error: #{e}"
+  sleep(10)
+  retry
+end
 require 'net/http'
 require 'hpricot'
 require 'open-uri'
@@ -31,15 +37,28 @@ class GroupPhotos
     
     # Open the config.yaml 
     config                  = File.open(CONFIG_PATH) { |yf| YAML::load( yf ) }
-    FlickRaw.api_key        = config["flickr_api_key"]["value"]
-    FlickRaw.shared_secret  = config["flickr_shared_secret"]["value"]
+    begin
+      FlickRaw.api_key        = config["flickr_api_key"]["value"]
+      FlickRaw.shared_secret  = config["flickr_shared_secret"]["value"]
+    rescue Exception => e
+      puts e
+      sleep(10)
+      retry
+    end
     
     # Create the save directory if it doesn't exist
     FileUtils.mkdir(@save_directory) unless File.exist?(@save_directory)
   end
   
+  #TODO CLEAN THIS CRAP UP!
   def pull_photos    
-    doc = Hpricot(open("http://www.flickr.com/groups/#{self.group_id}/pool/"))
+    begin
+      doc = Hpricot(open("http://www.flickr.com/groups/#{self.group_id}/pool/"))
+    rescue Exception
+      puts "Error when trying to open the flickr pool URL to get total number of images in this pool. Will retry..."
+      sleep(10)
+      retry
+    end
 
     total_number_of_photos = doc.search("//div[@class='Results']").inner_html.gsub(/[a-zA-Z,\(\)]/,"").to_f
     per_page = 500  
@@ -48,18 +67,45 @@ class GroupPhotos
     page = 1
     
     while times_to_loop <= flickr_request_looper
-      group_photos = flickr.groups.pools.getPhotos(:group_id  => self.group_id, 
-                                                   :per_page  => per_page,
-                                                   :page      => page)
+      begin
+        group_photos = flickr.groups.pools.getPhotos(:group_id  => self.group_id, 
+                                                     :per_page  => per_page,
+                                                     :page      => page)
+      rescue Exception
+        puts "Connection error when trying 'flickr.groups.pools.getPhoto()'. Will retry in 10 seconds..."
+        sleep(10)
+        retry
+      end
+      
       group_photos.each do |photo|
         begin
-          available_image_sizes = flickr.photos.getSizes(:photo_id => photo.id)
+          
+          # TODO make this its own method with proper error handeling 
+          begin
+            available_image_sizes = flickr.photos.getSizes(:photo_id => photo.id)
+          rescue Exception
+            puts "Connection error when trying 'flickr.photos.getSizes'. Will retry..."
+            sleep(10)
+            retry
+          end
+          
           original_image_url = available_image_sizes.find {|x| x.label == "Original"}.source.gsub("\\","").gsub(" ", "")
+          
           unless File.exist?(File.join(self.save_directory,File.basename(original_image_url)))
             puts "Downloading: #{original_image_url}"
-            url = URI.parse(original_image_url)
-            req = Net::HTTP::Get.new(url.path)
-            res = Net::HTTP.start(url.host, url.port) {|http|http.request(req)}
+
+            # TODO make this its own method with proper error handeling 
+            begin
+              url = URI.parse(original_image_url)
+              req = Net::HTTP::Get.new(url.path)
+              res = Net::HTTP.start(url.host, url.port) {|http|http.request(req)}
+            rescue Exception
+              puts "Connection error when trying to download the image. Will retry..."
+              sleep(10)
+              retry
+            end
+            
+            # TODO make this its own method with proper error handeling 
             File.open(File.join(self.save_directory,File.basename(original_image_url)), 'wb') do |file|
               file.write(res.body) # write the image file
             end
@@ -68,7 +114,12 @@ class GroupPhotos
             puts "Skipping #{original_image_url}"
           end
         rescue NoMethodError => e
+          puts "----------------------- MAJOR ERROR -------------------"
           puts e
+          puts "...will retry..."
+          puts "--------------------- END MAJOR ERROR -----------------"
+          sleep(10)
+          retry
         end
       end
       page += 1
